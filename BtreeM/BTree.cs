2fff3b9d01caaddcb9467437ed78bd176ff360bd
AnastasiiaLopata <lopataA@users.noreply.github.com>
2021-08-27 17:32:34 +0300

    Add project files.

---
+++ BtreeM/BTree.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtreeM
+{
+    class BTree
+    {
+        Node root = new Node();
+        pablic int T;
+
+        public BTree(int t)
+        {
+            T = t;
+            root = new Node();
+            root.countEl = 0;
+            root.leaf = true;
+
+        }
+        // search key
+
+        public Node search(Node x, int key)
+        {
+            int i = 0;
+            if (x == null)
+                return x;
+
+            for (i = 0; i < x.countEl; i++)
+            {
+                if (key < x.keyData[i])
+                {
+                    break;
+                }
+                if (key == x.keyData[i])
+                {
+                    return x;
+                }
+            }
+
+            if (x.leaf)
+            {
+                return null;
+            }
+            else
+            {
+                return search(x.child[i], key);
+            }
+
+
+        }
+        public void split(Node x,int pos, Node y)
+        {
+            Node z = new Node();
+            z.leaf = y.leaf;
+            z.countEl = 4-1;
+            for (int i = 0; i < T-1; i++)
+            {
+                z.keyData[i] = y.keyData[i + T];
+            }
+            if (!y.leaf)
+            {
+                for (int i = 0; i < T; i++)
+                {
+                    z.child[i] = y.child[i + T];
+
+                }
+            }
+            y.countEl = T - 1;
+            for (int i = x.countEl; i < length; i++)
+            {
+
+            }
+        }
+    }
+}
---
+++ BtreeM/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BtreeM
+{
+    class Node
+    {
+        int size;// input size of length data in node
+        public int countEl;// order of the tree n
+        public int[] keyData;
+        public Node[] child;
+        public bool leaf;
+
+        public Node()
+        {
+            size = 4;
+            keyData = new int[size];
+            child = new Node[size + 1];
+            leaf = true;
+        }
+
+        public int find(int k)
+        {
+            for (int i = 0; i < this.countEl; i++)
+            {
+                if (this.keyData[i] == k)
+                    return i;
+            }
+            return -1;
+
+        }
+
+
+
+
+
+    }
+}
